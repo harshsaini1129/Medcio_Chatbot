7d0af5089f2c44debdab50caa6e6e1f68ca4cd99
Harsh Saini <harshsaini1129@gmail.com>
2026-02-12 00:27:30 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# make directory
+mkdir -p src 
+mkdir -p research
+
+# create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch stetup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully!"
