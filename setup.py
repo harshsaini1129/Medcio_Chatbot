318a3b1aee185ec807115a0d4e5794eea5575b62
Harsh Saini <harshsaini1129@gmail.com>
2026-02-12 01:45:16 +0530

    update

---
+++ setup.py
@@ -0,0 +1,8 @@
+#for setup related code
+from setuptools import setup, find_packages
+setup(
+    name='medcio_chatbot',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[]
+)
--- template.sh
+++ template.sh
@@ -7,7 +7,7 @@ touch src/__init__.py
 touch src/helper.py
 touch src/prompt.py
 touch .env
-touch stetup.py
+touch setup.py
 touch app.py
 touch research/trials.ipynb
 touch requirements.txt
